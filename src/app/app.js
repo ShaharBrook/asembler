29e1a82f7b4ea3f5339502448af80e2d0b8a7419
Shahar <bro.shahar@gmail.com>
2021-06-25 14:58:51 +0300

    remove fluff and basic beginning

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import './app.css';
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app full-container">
+      <div className='code'>
+        code
+      </div>
+    </div>
+  );
+};
+
+export default App;
